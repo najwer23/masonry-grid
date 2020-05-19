f101a7d053d751f28cadd5ea9382c1504d115b53
Travis CI <najwer23@gmail.com>
2020-05-19 18:16:47 +0200

    Obiektowe podejście do problemu

--- JS/masonry.js
+++ JS/masonry.js
@@ -1,23 +1,55 @@
-window.onload = function () {
-    allItems = document.querySelectorAll(".item");
-
-    function resizeGridItem(item) {
-        grid = document.querySelector(".grid");
-        rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-        rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-        rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
-        
-        item.style.gridRowEnd = "span " + rowSpan;
+class MasonryGrid {
+    constructor(params) {
+        this.grid = document.querySelector(params.grid);
+        this.items = document.querySelectorAll(params.items);
+        this.content = params.content;
     }
 
-    function resizeAllGridItems() {
-        allItems = document.querySelectorAll(".item");
-        for (x = 0; x < allItems.length; x++) {
-            resizeGridItem(allItems[x]);
+    resizeAllGridItems() {
+        for (let i = 0; i < this.items.length; i++) {
+            this.resizeGridItem(this.items[i]);
         }
     }
 
-    resizeAllGridItems()
-    window.addEventListener("resize", resizeAllGridItems);
-
+    resizeGridItem(item) {
+        let rowHeight = parseInt(window.getComputedStyle(this.grid).getPropertyValue('grid-auto-rows'));
+        let rowGap = parseInt(window.getComputedStyle(this.grid).getPropertyValue('grid-row-gap'));
+        let rowSpan = Math.ceil((item.querySelector(this.content).getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+        item.style.gridRowEnd = "span " + rowSpan;
+        item.style.visibility = "visible";
+    }
 }
+
+window.onload = function () {
+    
+    // IN params
+    let masonryGrid;
+    let masonryGridArr = new Array();
+
+    const masonryGridParams = [
+        {
+            grid: "#mansoryGrid",
+            items: ".item",
+            content: ".content"
+        }
+    ];
+
+    // Create Classes
+    for (let i=0; i<masonryGridParams.length; i++) {
+        masonryGrid = new MasonryGrid(masonryGridParams[i])
+        masonryGrid.resizeAllGridItems();
+        masonryGridArr.push(masonryGrid);   
+    }
+
+    //Events
+    window.addEventListener("resize", function() {
+        masonryGridArr.forEach(function(elem) {
+            elem.resizeAllGridItems();
+        });
+    });
+}
+
+
+// Special thanks:
+// https://css-tricks.com/piecing-together-approaches-for-a-css-masonry-layout/
+// https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
