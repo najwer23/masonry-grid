39d1b8db01b9e5a6e1c377242c7d1719c1919909
Mariusz Najwer <najwer23@gmail.com>
2021-11-21 20:59:16 +0100

    Rebuild webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
+
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var entryCustom = {};
+let entryArr = ["style", "index"];
+for (let i = 0; i < entryArr.length; i++) {
+  entryCustom[entryArr[i]] = path.resolve(__dirname, './assets/js/' + entryArr[i] + '.js')
+}
 
 module.exports = {
-  entry: {
-    app: [
-      '@babel/polyfill',
-      './assets/js/index.js'
-    ],
-    styleMain: [
-      './assets/js/style.js'
-    ]
-  },
+  entry: entryCustom,
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bunde.js'
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          type: "css/mini-extract",
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
+    minimizer: [
+      '...',
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      linkType: "text/css",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: "./assets/img/f.png",
+    })
+  ],
   module: {
     rules: [
       {
@@ -26,19 +51,27 @@ module.exports = {
         }
       },
       {
-        test: /\.(sass|scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader']
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ['file-loader']
-      }
+        test: /\.(sass|scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/img/',
+              publicPath: 'assets/img/'
+            }
+          }
+        ]
+      },
     ]
-  }
+  },
 }
-
-// https://github.com/webpack/webpack/issues/1732
