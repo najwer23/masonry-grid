003adf8409818d5d9f4a599a64cccf661f01d687
Mariusz Najwer <najwer23@gmail.com>
2021-11-22 16:47:43 +0100

    Refractoring

--- assets/js/masonry-grid.js
+++ assets/js/masonry-grid.js
@@ -1,134 +1,153 @@
-var currentColumns = -1
-var percentScroll;
+var CURRENT_NUMBER_OF_COLUMNS = -1
+var SCROLL_PERCENT_POSITION;
 
-//In
-var masonryParent = document.querySelector("#masonry")
+// IN - SET ID OF Masonry
+let MASONRY_ARR_ID = [
+    "masonry"
+]
 
-window.onload = () => {
-    makeMasonryLayout(masonryParent)
-};
+
+
+
+
+window.addEventListener('load', (event) => {
+    setMasonry();
+});
 
 window.addEventListener('resize', function(){
-    makeMasonryLayout(masonryParent)
-    var Ypos = document.body.scrollHeight * percentScroll;
-    window.scrollTo(0, Ypos);
+    setMasonry();
+    //remember scroll position when resize
+    window.scrollTo(0, document.body.scrollHeight * SCROLL_PERCENT_POSITION);
 })
 
-window.addEventListener("scroll", getScrollPercent);
-function getScrollPercent() {
-    percentScroll = window.pageYOffset / document.body.scrollHeight;
+window.addEventListener("scroll", function(){
+    SCROLL_PERCENT_POSITION = window.pageYOffset / document.body.scrollHeight;
+});
+
+
+
+
+
+
+
+function setMasonry() {
+    for (let i=0; i<MASONRY_ARR_ID.length; i++) {
+        makeMasonryLayout(document.querySelector("#" + MASONRY_ARR_ID[i]))
+    }
 }
 
-function makeMasonryLayout(masonryParent) {
-    addCss2Element(masonryParent, {
-    })
-
-    let masonryChilds = masonryParent.querySelectorAll('[id^="masonry-child-"]');
-    let masonryParentWidth = masonryParent.clientWidth;
-    let masonryParentColumnsDictionary = JSON.parse(masonryParent.getAttribute("columns"))
-    let masonryParentColumnsSpace = masonryParent.getAttribute("space")
-
-    let masonryNumberOfColumns = findNumberOfColumns(masonryParentWidth, masonryParentColumnsDictionary)
+function makeMasonryLayout(masonryParentEle) {
+    let masonryChilds = masonryParentEle.querySelectorAll('[id^="masonry-child-"]');
+    let masonryParentEleWidth = masonryParentEle.clientWidth;
+    let masonrySpace = masonryParentEle.getAttribute("space");
+    let masonrySpaceHalf = masonrySpace/2;
+    let masonryNumberOfColumns = findNumberOfColumns(
+        masonryParentEleWidth, 
+        JSON.parse(masonryParentEle.getAttribute("columns"))
+    )
 
     //set columns
-    if (currentColumns != masonryNumberOfColumns) {
-
+    if (CURRENT_NUMBER_OF_COLUMNS != masonryNumberOfColumns) {
+        
         //reset child position in columns
         for (let i = 0; i < masonryChilds.length; i++) {
-            masonryParent.appendChild(document.querySelector("#masonry-child-"+i))
+            masonryParentEle.appendChild(masonryParentEle.querySelector("#masonry-child-"+i))
         }
 
         //remove old columns
-        let divColumnToRemove = document.querySelectorAll('[id^="masonry-col-"]')
-        for (let i = 0; i < divColumnToRemove.length; i++) {
-            masonryParent.removeChild(divColumnToRemove[i])
+        let masonryColumnsToRemove = document.querySelectorAll('[id^="masonry-col-"]')
+        for (let i = 0; i < masonryColumnsToRemove.length; i++) {
+            masonryParentEle.removeChild(masonryColumnsToRemove[i])
         }
 
+        //create new columns
         let paddingLeft = 0;
         let paddingRight = 0;
         let paddingBottom = 0;
-        //create new columns
-        for (let i = 0; i < masonryNumberOfColumns; i++) {
-            let newDivColumn = document.createElement("div");
-            let newDivId = "masonry-col-" + i
-            newDivColumn.id = newDivId;
-            masonryParent.appendChild(newDivColumn)
+        let newMasonryColumn;
+        let paddingLeftRight;
 
-            //first and last column
-            if ((i%(masonryNumberOfColumns-1)) == 0) {
-                if (i==0) {
-                    paddingLeft = 0;
-                    paddingRight = masonryParentColumnsSpace / 2;
-                } else {
-                    paddingLeft = masonryParentColumnsSpace /2;
-                    paddingRight = 0;
-                }
+        for (let i = 0; i < masonryNumberOfColumns; i++) {
+            newMasonryColumn = document.createElement("div");
+            newMasonryColumn.id = "masonry-col-" + i;
+            masonryParentEle.appendChild(newMasonryColumn)
+
+            //set padding for first and last column
+            if ((i % (masonryNumberOfColumns-1)) == 0) {
+                paddingRight = (i==0) ? masonrySpaceHalf : 0;
+                paddingLeft = (i!=0) ? masonrySpaceHalf : 0;
             } else {
-                paddingLeft = masonryParentColumnsSpace / 2
-                paddingRight = masonryParentColumnsSpace / 2
+                paddingLeft = masonrySpaceHalf
+                paddingRight = masonrySpaceHalf
             }
 
-            // zero if one column
+            //set zero padding if masonry has one column
             if (masonryNumberOfColumns == 1) {
                 paddingLeft = 0;
                 paddingRight = 0;
             }
 
-            let paddingWidth = paddingLeft + paddingRight;
-            addCss2Element(newDivColumn, {
-                'background-color': 'yellow',
-                'color': 'red',
+            //set padding in column
+            paddingLeftRight = paddingLeft + paddingRight;
+            addCss2Element(newMasonryColumn, {
+                "background": "inherit",
                 'float': "left",
-                'width': `calc((100% / ${masonryNumberOfColumns}) - ${paddingWidth+"px"})`,
+                'width': `calc((100% - ${paddingLeftRight + "px"}) / ${masonryNumberOfColumns} )`,
                 'padding': `0 ${paddingRight+"px"} ${paddingBottom+"px"} ${paddingLeft+"px"}` 
             })
-            currentColumns = masonryNumberOfColumns
+
+            // update number of columns
+            CURRENT_NUMBER_OF_COLUMNS = masonryNumberOfColumns
         }
     }
 
-    // set height for columns
-    var allCurrentColumnsId = {};
-    let allCurrentColumnsIdNodeList = document.querySelectorAll('[id^="masonry-col-"]')
-    for (let i = 0; i < allCurrentColumnsIdNodeList.length; i++) {
-        allCurrentColumnsId[allCurrentColumnsIdNodeList[i].id] = 0;
+    // set starting yheight for columns
+    var masonryColumnsCurrentHeight = {};
+    let masonryColumnsCurrent = masonryParentEle.querySelectorAll('[id^="masonry-col-"]')
+    for (let i = 0; i < masonryColumnsCurrent.length; i++) {
+        masonryColumnsCurrentHeight[masonryColumnsCurrent[i].id] = 0;
     }
 
     //insert childs in new columns
-    for (let i=0; i<masonryChilds.length; i++) {
-        let heightChild = document.querySelector("#masonry-child-" + i).clientHeight;
+    let masonryChildHeight;
+    let masonryColumnsCurrentHeightKeys = Object.keys(masonryColumnsCurrentHeight)
+    let masonryColumnMinHeightName = "";
 
-        let allCurrentColumnsIdKeys = Object.keys(allCurrentColumnsId)
-        let colMinHeightName = ""
-        let colMinHeightValue = Infinity
-        
-        for (let j = allCurrentColumnsIdKeys.length-1; j>=0; j--) {
-            if (allCurrentColumnsId[allCurrentColumnsIdKeys[j]] <= colMinHeightValue) {
-                colMinHeightName = allCurrentColumnsIdKeys[j];
-                colMinHeightValue = allCurrentColumnsId[allCurrentColumnsIdKeys[j]]
+
+    for (let i=0; i<masonryChilds.length; i++) {
+        let masonryChildEle = masonryParentEle.querySelector("#masonry-child-" + i)
+        masonryChildHeight = masonryChildEle.clientHeight;
+
+        let masonryColumnMinHeightValue = Infinity
+        for (let j = masonryColumnsCurrentHeightKeys.length-1; j>=0; j--) {
+            if (masonryColumnsCurrentHeight[masonryColumnsCurrentHeightKeys[j]] <= masonryColumnMinHeightValue) {
+                masonryColumnMinHeightName = masonryColumnsCurrentHeightKeys[j];
+                masonryColumnMinHeightValue = masonryColumnsCurrentHeight[masonryColumnsCurrentHeightKeys[j]]
             }
         }
 
-        allCurrentColumnsId[colMinHeightName] += heightChild;
-        let ele = document.querySelector("#masonry-child-" + i)
-        addCss2Element(ele, {
-            'margin-bottom': `${masonryParentColumnsSpace + "px"}`
+        masonryColumnsCurrentHeight[masonryColumnMinHeightName] += masonryChildHeight;
+        addCss2Element(masonryChildEle, {
+            'margin-bottom': `${masonrySpace + "px"}`
         })
-        document.querySelector("#" + colMinHeightName).appendChild(ele)
+        masonryParentEle.querySelector("#" + masonryColumnMinHeightName).appendChild(masonryChildEle)
     }
 }
 
-function findNumberOfColumns(currentWidth, dictionary) {
+function findNumberOfColumns(parentWidth, dictionary) {
     let dictionaryKeys = Object.keys(dictionary).map(v=>+v).sort((a,b)=>a-b)
+
+    // set default columns
     let numberOfColumns = 4; 
 
     for (let i=0; i<dictionaryKeys.length; i++) {
-        if (currentWidth <= dictionaryKeys[i]) {
+        if (parentWidth <= dictionaryKeys[i]) {
             numberOfColumns = dictionary[dictionaryKeys[i]]
             break;
         }
     }
 
-    return numberOfColumns
+    return numberOfColumns;
 }
 
 function addCss2Element(element, style) {
