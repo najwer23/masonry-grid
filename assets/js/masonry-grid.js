43d079ceab1c0366efb3e9a67eb4ed05ab532d74
Mariusz Najwer <najwer23@gmail.com>
2021-11-22 14:38:40 +0100

    Refractoring

--- assets/js/masonry-grid.js
+++ assets/js/masonry-grid.js
@@ -1,10 +1,3 @@
-// let masonry = document.querySelector("#masonry")
-// let masonryChilds = document.querySelectorAll("#masonry > *")
-// let bodyClientWidth = document.body.clientWidth;
-// let masonryNumOfColumnsJSON = JSON.parse(masonry.getAttribute("columns"))
-// let masonryNumOfColumns = 1;
-
-
 var currentColumns = -1
 var percentScroll;
 
@@ -95,8 +88,6 @@ function makeMasonryLayout(masonryParent) {
     }
 }
 
-
-
 function findNumberOfColumns(currentWidth, dictionary) {
     let dictionaryKeys = Object.keys(dictionary).map(v=>+v).sort((a,b)=>a-b)
     let numberOfColumns = 4; 
@@ -110,19 +101,6 @@ function findNumberOfColumns(currentWidth, dictionary) {
 
     return numberOfColumns
 }
-
-
-
-
-// masonryParents[minIndex].element.appendChild(masonryChilds.item(i))
-
-
-// addCss(newColumnDiv, {
-//     'background-color': 'yellow',
-//     'color': 'red',
-//     'width': '50%'//`${masonryColumnWidth-masonryNumOfColumns*5}px`
-// })
-
 
 function addCss2Element(element, style) {
     for (const property in style) {
